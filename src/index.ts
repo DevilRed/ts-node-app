eed004909bc046d5ba6a3b7ccd47f164e16d82a3
Christian <chrisdaq@gmail.com>
2019-08-22 14:27:50 -0400

    install @types/express-handlebars, tslint, ts-node-dev (for typescript development), set views, handlebars config, set static files directory

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
 
 // initializations
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+// set xonfig for handlebars in express
+app.engine('.hbs', exphbs({
+	extname: '.hbs',
+	layoutsDir: path.join(app.get('views'), 'layouts'),
+	partialsDir: path.join(app.get('views'), 'partials'),
+	helpers: require('./lib/helpers')
+}));
+// use handlebars config
+app.set('view engine', '.hbs');
+
 // middlewares
+app.use(express.json());// enable json in express server
+app.use(express.urlencoded({extended: false}));// enable data from html form
+// app.
 
 // routes
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // starting the server
 app.listen(app.get('port'), () => {
