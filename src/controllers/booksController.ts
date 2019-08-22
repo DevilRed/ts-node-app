c269f964101477e9808fc83d96b410395ebe469a
Christian <chrisdaq@gmail.com>
2019-08-22 19:48:59 -0400

    retrieve books from db and show them in books/index

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -7,15 +7,20 @@ class BooksController {
     res.render('books/add', { title: 'Add a book'});
   }
 
-  public index(req: Request, res: Response): void {
-    res.render('books/index', { title: 'Books'});
+  public async index(req: Request, res: Response): Promise<void> {
+     // get all books using the model
+    const books: IBook[] = await Book.find();
+    res.render('books/index', {
+      title: 'Books',
+      books,
+    });
   }
 
   public renderFormBook(req: Request, res: Response): void {
     res.render('books/add', { title: 'Add a book'});
   }
 
-  public async saveBook(req: Request, res: Response): void {
+  public async saveBook(req: Request, res: Response): Promise<void> {
     const {title, author, isbn} = req.body;
     // pass an object with the data from req
     const book: IBook = new Book({ title, author, isbn });
